77a92258803ba5aaa91a07af6dfb395e418efa5d
Andy Miller <onetribeyoyo@gmail.com>
2019-07-28 18:17:55 -0500

    cleaned out all the irrelevant crap.

---
+++ dev_orig/colormap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DEV_DIR=~/github/onetribeyoyo/mandleview/dev_orig
+PROJECT_DIR=$DEV_DIR/planview
+LIB_DIR=$PROJECT_DIR/distribution/lib
+JAR=$LIB_DIR/planview-DEV.jar
+MAIN=amiller.colormap.Main
+
+#echo jar file: $JAR
+#echo class:    $MAIN
+
+java -Xmx256m -cp $JAR:$LIB_DIR/log4j-1.2.6.jar $MAIN $@
